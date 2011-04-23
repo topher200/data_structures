6d22b17cd5b6103ce8e1d4f218494950f6ab8832
Topher Brown <topher200@gmail.com>
2011-04-23 16:30:48 -0400

    initial commit

---
+++ hash_table.py
@@ -0,0 +1,39 @@
+num_buckets = 10
+
+class HashTable():
+  def __init__(self):
+    self.table = []
+    for i in range(num_buckets):
+      self.table.append([])
+
+  def add(self, element):
+    self.table[self._hash_function(element.key)].append(element)
+
+  def find(self, key):
+    for element in self.table[self._hash_function(key)]:
+      if element.key == key:
+        return element
+
+  def output(self):
+    for i, bucket in enumerate(self.table):
+      print '{0}:'.format(i),
+      for element in bucket:
+        print element.output(),
+      print 
+
+  @staticmethod
+  def _hash_function(string):
+    # map/reduce?
+    sum = 0
+    for char in string:
+      sum += ord(char)
+    return sum % num_buckets
+
+
+class Element():
+  def __init__(self, key, value):
+    self.key = key
+    self.value = value
+
+  def output(self):
+    return('[{0} {1}]'.format(self.key, self.value))
---
+++ tester.py
@@ -0,0 +1,33 @@
+import random
+from hash_table import HashTable, Element
+
+def generate_random_key():
+  length = random.randint(1, 10)
+  string_as_int_array = []
+  for _ in range(length):
+    string_as_int_array.append(random.randint(97, 122))
+  return ''.join(map(chr, string_as_int_array))
+
+def generate_random_value():
+  return random.randint(1, 1000)
+
+def random_element():
+  return Element(generate_random_key(), generate_random_value())
+
+def main():
+  # Create hash table
+  h = HashTable()
+
+  # Populate it
+  for _ in range(100):
+    h.add(random_element())
+  h.add(Element('topher', 1337))
+
+  # Print it the table
+  print(h.output())
+
+  # Try a find
+  print(h.find('topher').output())
+
+if __name__ == '__main__':
+  main()
